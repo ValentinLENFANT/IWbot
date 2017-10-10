05f34eb38b7b13d98ec973f9cea45cae900fe655
ValentinLenfant <valentin.lenfant@hotmail.com>
2017-10-10 20:51:10 +0200

    Ajout TP 2

--- app.js
+++ app.js
@@ -4,66 +4,121 @@
 
 var restify = require('restify');
 var botbuilder = require('botbuilder');
-var savedAddress;
 
-// Setup restify server
+// restify server setup
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3987, function () {
-    console.log('%s Bot started at %s', server.name, server.url);
+server.listen(process.env.port || process.env.PORT || 3987, function(){
+    console.log(`${server.name} bot started at ${server.url}`);
 });
 
-// Create chat connector
+// create chat connector
 var connector = new botbuilder.ChatConnector({
     appId: process.env.APP_ID,
     appPassword: process.env.APP_SECRET
 });
 
-// Listening for user inputs
+// listening for user inputs
 server.post('/api/messages', connector.listen());
 
-// Reply by echoing
-var bot = new botbuilder.UniversalBot(connector, function (session) {
-    savedAddress = session.message.address;
-    session.send('You have tapped the following message: %s <br /> It has a length of: %s ', session.message.text, session.message.text.length); //lol <br /> is working
-    // session.send(`You have tapped:${session.message.text} | [Length: ${session.message.text.length}] `);
+// reply by echoing
+var bot = new botbuilder.UniversalBot(connector, [
+    function(session) {
+        session.beginDialog('ensureProfile');
 
-});
+        // bot.on('typing', function(response) {
+        //     session.send(response); // Rajouter cette ligne corrige le Bug de messages dupliqués
+        //     session.send("ah tu es encore en train de taper..");
+        // });
 
-// User is typing
-bot.on('typing', function () {
-    bot.send(new botbuilder.Message().address(savedAddress).text('You are typing slowly...'));
-    //session.send('You are typing slowly...');
-});
-
-// Welcome the user on startup + when user added
-bot.on('conversationUpdate', function (message) {
-    if (message.membersAdded && message.membersAdded.length > 0) {
-        message.membersAdded.map(function (x) {
-
-            // Check if not a bot
-            if (x.id != message.address.bot.id) {
-
-                // User info
-                var membersAdded = x.name || ' ' + '(Id=' + x.id + ' )';
-
+        bot.on('conversationUpdate', function(message) {
+            if(message.membersAdded && message.membersAdded.length > 0) {
+                var membersAdded = message.membersAdded.map(function (x) {
+                    var isSelf = x.id === message.address.bot.id;
+                    return (isSelf ? message.address.bot.name : x.name) || ' ' + 'Id = ' + x.id + ')'
+                }).join(', ');
                 bot.send(new botbuilder.Message()
                     .address(message.address)
-                    .text('Welcome ' + membersAdded + '! How can I help you today? ')
+                    .text('Bienvenue' + membersAdded)
                 );
             }
-        }).join(', ');
+        });
+    },
+    function(session, generic) {
+        botbuilder.Prompts.text(session, `${generic.temp}`);
+        session.endConversation("See you soon!");
     }
-});
+]);
 
-// Add and delete bot
-bot.on('contactRelationUpdate', function (message) {
-    var isSelf = message.address.bot.name + '-' + message.address.bot.id;
+bot.dialog('greetings', [
+    function(session) {
+        session.beginDialog('askName');
+    },
+    function(session, generic) {
+        session.endDialogWithResult(generic);
+    }
+]);
 
-    bot.send(new botbuilder.Message()
-        .address(savedAddress)
-        .text('Welcome ' + isSelf));
-});
+bot.dialog('askName', [
+    function(session) {
+        botbuilder.Prompts.text(session, "Hi, what's your name ?");
+    },
+    function(session, results) {
+        var temp = "Hello mate!";
+        var generic = {
+            temp: temp,
+            results: results
+        };
+        session.endDialogWithResult(generic);
+    }
+]);
 
-//todo create a git repository, create a new branch to identify the TP (by example TP-12/09/2017), put the code in it.
-//chatbot must echo the input text. He can also detects when a person is typing. The bot give the information of a new user but also for a bot. Same thing for the removal.
-//welcome message when loging to a chatbot
+bot.dialog('ensureProfile', [
+    function(session, args, next) {
+        session.dialogData.profile = args || {};
+        if(!session.dialogData.profile.name){
+            botbuilder.Prompts.text(session, "What is your name?");
+        } else {
+            next();
+        }
+    },
+    function (session, results, next) {
+        if(results.response) {
+            session.dialogData.profile.name = results.response;
+        }
+        if(!session.dialogData.profile.company) {
+            botbuilder.Prompts.text(session, "What company do you work for?");
+        } else {
+            next();
+        }
+    },
+    function(session, results) {
+        if(results.response){
+            session.dialogData.profile.company = results.response;
+        }
+        session.endDialogWithResult({response: session.dialogData.profile})
+    }
+]);
+
+bot.dialog('dinnerOrder', [
+    function(session, results){
+        if(results.response){
+            session.dialogData.room = results.response;
+            var msg = `Thank you. Your order will be delivered to room #${session.dialogData.room}`;
+            session.endConversation(msg);
+        }
+    }
+])
+.endConversationAction(
+    "endOrderDinner", "Ok. Goodbye.",
+    {
+        matches: /^cancel$|^goodbye$/i,
+        confirmPrompt: "This will cancel your order. Are you sure?"
+    }
+);
+
+//Bot de réservation pour manger dans un restaurant
+//Greeting et récupération de nom
+//1) Date de la réservation
+//2) Le nombre de personnes
+//3) Le nom de la personne qui porte la réservation
+//End --> Le bot me confirme la réservation avec toutes les informations
